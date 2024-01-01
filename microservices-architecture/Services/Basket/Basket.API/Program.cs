c25a63cc9bba5313a56e3375efd42aa562148d7f
Muneeb Ghayas <muneeb@globcom-oman.com>
2024-01-01 09:06:10 +0400

    discount grpc

--- microservices-architecture/Services/Basket/Basket.API/Program.cs
+++ microservices-architecture/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,20 @@
+using Basket.API.GrpcServices;
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
+using Discount.Grpc.Protos;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+    (o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
+builder.Services.AddScoped<DiscountGrpcService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
